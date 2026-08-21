8d49d3393092d2ebff56e6162dfee43126c34416
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-08-21 17:07:06 +0200

    getmodfilebyid

--- src/net/neoforged/fml/loading/LoadingModList.java
+++ src/net/neoforged/fml/loading/LoadingModList.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public abstract class LoadingModList {
     public abstract List<ModFileInfo> getModFiles();
+    public abstract ModFileInfo getModFileById(String modid);
 }

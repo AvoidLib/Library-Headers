8c3e48811a3e3160f7da9cb3d009413e94d30d1f
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-08-24 17:28:41 +0200

    [NeoForge] @OnlyIn

---
+++ NeoForge Headers/src/neoforged/api/distmarker/OnlyIn.java
@@ -0,0 +1,9 @@
+package neoforged.api.distmarker;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.CLASS)
+public @interface OnlyIn {
+    Dist value();
+}

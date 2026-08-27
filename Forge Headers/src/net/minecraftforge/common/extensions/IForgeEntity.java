86a66fcc87816bf4b80727d9baa2d1abfde52e67
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-08-27 12:50:10 +0200

    IForgeEntity#isInFluidType

--- Forge Headers/src/net/minecraftforge/common/extensions/IForgeEntity.java
+++ Forge Headers/src/net/minecraftforge/common/extensions/IForgeEntity.java
@@ -1,4 +1,9 @@
 package net.minecraftforge.common.extensions;
 
+import net.minecraftforge.fluids.FluidType;
+
+import java.util.function.BiPredicate;
+
 public interface IForgeEntity {
+    boolean isInFluidType(BiPredicate<FluidType, Double> predicate, boolean forAllTypes);
 }
